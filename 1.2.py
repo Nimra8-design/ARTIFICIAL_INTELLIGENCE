240b2823851210b9664fc7fd8e7a8281d9ea1be2
Nimra8-design <nimrasaad108@gmail.com>
2025-03-17 12:29:24 -0700

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 12:37:42 2025
+
+@author: cuty computer
+"""
+
+#Comments in python and if condition
+
+x=1
+
+#The initial value of x is 1.
+
+if x>0:
+    print("There are two comments")  #Print a sring
+    
