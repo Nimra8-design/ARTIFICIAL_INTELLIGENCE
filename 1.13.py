34367c05f9bfa1b2fd6601d4c778bfd9f0c68771
Nimra8-design <nimrasaad108@gmail.com>
2025-03-17 13:16:03 -0700

    Add files via upload

---
+++ 1.13.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 14:12:41 2025
+
+@author: cuty computer
+"""
+
+#list slices
+
+list3=[9.8,0,7.8,3,4,5,'hello']
+    
+print(list3[3:5])
+print(list3[6:-5])
+print(list3[2])
+print(list3[0:4],list3[6:-4])
+
+#Creates copy of original list
+print(list3[:])
