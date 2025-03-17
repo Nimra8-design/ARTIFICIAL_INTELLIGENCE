44758e734df6bd344f31a8c5e475b2bca76f1deb
Nimra8-design <nimrasaad108@gmail.com>
2025-03-17 12:31:18 -0700

    Add files via upload

---
+++ 1.5.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 13:06:16 2025
+
+@author: cuty computer
+"""
+
+#indentation
+x=1
+if x>0:
+    print("This statement has a single space indentation.")
+    print("This statement has a single space indentation.")
+    
+    
