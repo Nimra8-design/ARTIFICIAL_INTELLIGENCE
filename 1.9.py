c38bf970e9919716a60e6f07d91c61ec63ba908a
Nimra8-design <nimrasaad108@gmail.com>
2025-03-17 12:55:42 -0700

    Add files via upload

---
+++ 1.9.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 13:50:52 2025
+
+@author: cuty computer
+"""
+
+#Special Characters in string
+
+print ("This is backslash (\\) mark")
+
+print("This is tab \t key")
+
+print("These are \'Single quotes\'")
+
+print("These are \"Double quotes\"")
+
+print("This is a new line \n New line")
