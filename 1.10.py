5d75904c6440ad48a61d6f868a113f3e05f23671
Nimra8-design <nimrasaad108@gmail.com>
2025-03-17 12:59:49 -0700

    Add files via upload

---
+++ 1.10.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 13:55:31 2025
+
+@author: cuty computer
+"""
+
+string1="Python Tutorial"
+
+print(string1[0])
+
+print(string1[-15])
+
+print(string1[14])
+
+print(string1[-1])
+
+print(string1[4])
+
+print(string1[-11])
+
+'''
+print(string1[16])
+
+syntax error: index out of range
+
+'''
